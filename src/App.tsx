7449b9abb3c990571bff40865630595310bf8224
pmiguel <pedromiguelnevesdemeu@gmail.com>
2022-05-10 21:32:54 -0300

    :tada: Finish configuration Styles for project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
