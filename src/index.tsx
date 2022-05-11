c52f2675049ecf3a7deda3bfb11ea67db4feec66
pmiguel <pedromiguelnevesdemeu@gmail.com>
2022-05-11 15:06:39 -0300

    :rocket: Feat: api configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { createServer } from "miragejs";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
@@ -5,6 +6,26 @@ import { App } from "./App";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
 root.render(
   <React.StrictMode>
     <App />
